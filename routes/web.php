a65bc4d13d3557ea2c8e1479fdf404d3070e485b
Kashif Rehman <kashifdeveloper9053@gmail.com>
2025-01-03 19:49:40 +0500

    again

--- routes/web.php
+++ routes/web.php
@@ -35,72 +35,118 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-// Redirect default route to user home
 Route::get('/', function () {
     return redirect()->route('user.home');
 });
+Route::prefix('/admin')->name('admin.')->group(function(){
+    Route::get('/sign',[AdminSignController::class,'login'])->name('sign');
+    Route::get('/checklogin',[AdminSignController::class,'checkLogin'])->name('checklogin');
 
-// Admin Routes
-Route::prefix('/admin')->name('admin.')->group(function () {
-    // Publicly accessible admin routes
-    Route::get('/sign', [AdminSignController::class, 'login'])->name('sign');
-    Route::get('/checklogin', [AdminSignController::class, 'checkLogin'])->name('checklogin');
-    Route::get('/signup', [AdminSignController::class, 'signup'])->name('signup');
-    Route::get('/checkregister', [AdminSignController::class, 'checkRegister'])->name('checkregister');
+    Route::get('/signup',[AdminSignController::class,'signup'])->name('signup');
+    Route::get('/checkregister',[AdminSignController::class,'checkRegister'])->name('checkregister');
 
-    // Protected admin routes
-    Route::middleware(['auth.admin'])->group(function () {
-        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
-        Route::get('/tour', [AdminTourController::class, 'index'])->name('tour');
-        Route::get('/schedule', [AdminScheduleController::class, 'index'])->name('schedule');
-        Route::get('/order', [AdminOrderController::class, 'index'])->name('order');
-        Route::get('/customer', [AdminCustomerController::class, 'index'])->name('customer');
-        Route::get('/staff', [AdminStaffController::class, 'index'])->name('staff');
-        Route::get('/news', [AdminNewsController::class, 'index'])->name('news');
-        Route::get('/places', [AdminPlacesController::class, 'index'])->name('places');
-        Route::get('/feedback', [AdminFeedbackController::class, 'index'])->name('feedback');
-        Route::get('/tourguide', [AdminTourguideController::class, 'index'])->name('tourguide');
-        Route::get('logout', [AdminSignController::class, 'logout']);
+    Route::get('/dashboard',[AdminDashboardController::class,'index'])->name('dashboard');
 
-        // Admin control routes
-        Route::prefix('/control')->name('control.')->group(function () {
-            Route::get('/insertTour', [AdminTourController::class, 'insertTour'])->name('insertTour');
-            Route::post('/insertTourPost', [AdminTourController::class, 'insertTourPost'])->name('insertTourPost');
-            Route::get('/updateTour/{id}', [AdminTourController::class, 'updateTour'])->name('updateTour');
-            Route::post('/updateTourPost', [AdminTourController::class, 'updateTourPost'])->name('updateTourPost');
-            Route::get('/deleteTour/{id}', [AdminTourController::class, 'deleteTour'])->name('deleteTour');
-            
-            // More admin control routes here...
-        });
+    Route::get('/tour',[AdminTourController::class,'index'])->name('tour');
+
+    Route::get('/schedule',[AdminScheduleController::class,'index'])->name('schedule');
+
+    Route::get('/order',[AdminOrderController::class,'index'])->name('order');
+
+    Route::get('/customer',[AdminCustomerController::class,'index'])->name('customer');
+
+    Route::get('/staff',[AdminStaffController::class,'index'])->name('staff');
+
+    Route::get('/news',[AdminNewsController::class,'index'])->name('news');
+
+    Route::get('/places',[AdminPlacesController::class,'index'])->name('places');
+
+    Route::get('/feedback',[AdminFeedbackController::class,'index'])->name('feedback');
+
+    Route::get('/tourguide',[AdminTourguideController::class,'index'])->name('tourguide');
+
+    Route::get('logout',[AdminSignController::class,'logout']);
+
+    Route::prefix('/control')->name('control.')->group(function(){
+        Route::get('/insertTour',[AdminTourController::class,'insertTour'])->name('insertTour');
+        Route::post('/insertTourPost',[AdminTourController::class,'insertTourPost'])->name('insertTourPost');
+        Route::get('/updateTour/{id}',[AdminTourController::class,'updateTour'])->name('updateTour');
+        Route::post('/updateTourPost',[AdminTourController::class,'updateTourPost'])->name('updateTourPost');
+        Route::get('/deleteTour/{id}',[AdminTourController::class,'deleteTour'])->name('deleteTour');
+
+        Route::get('insertSchedule',[AdminScheduleController::class,'insertSchedule'])->name('insertSchedule');
+        Route::get('insertSchedulePost',[AdminScheduleController::class,'insertSchedulePost'])->name('insertSchedule');
+        Route::get('updateSchedule/{id}',[AdminScheduleController::class,'updateSchedule'])->name('updateSchedule');
+        Route::get('updateSchedulePost',[AdminScheduleController::class,'updateSchedulePost'])->name('updateSchedule');
+        Route::get('deleteSchedule/{id}',[AdminScheduleController::class,'deleteSchedule'])->name('deleteSchedule');
+
+        Route::get('changeOn/{id}',[AdminOrderController::class,'changeOn']);
+        Route::get('changeOff/{id}',[AdminOrderController::class,'changeOff']);
+        Route::get('deleteOrder/{id}',[AdminOrderController::class,'deleteOrder']);
+
+
+        Route::get('insertNews',[AdminNewsController::class,'insertNews']);
+        Route::post('insertNewsPost',[AdminNewsController::class,'insertNewsPost']);
+        Route::get('updateNews/{id}',[AdminNewsController::class,'updateNews']);
+        Route::post('updateNewsPost',[AdminNewsController::class,'updateNewsPost']);
+        Route::get('deleteNews/{id}',[AdminNewsController::class,'deleteNews']);
+
+        Route::get('lock/{id}',[AdminCustomerController::class,'lock']);
+        Route::get('unlock/{id}',[AdminCustomerController::class,'unlock']);
+        Route::get('deleteCustomer/{id}',[AdminCustomerController::class,'deleteCustomer']);
+
+        Route::get('lockStaff/{id}',[AdminStaffController::class,'lock']);
+        Route::get('unlockStaff/{id}',[AdminStaffController::class,'unlock']);
+
+        Route::get('insertPlaces',[AdminPlacesController::class,'insertPlaces']);
+        Route::post('insertPlacesPost',[AdminPlacesController::class,'insertPlacesPost']);
+        Route::get('updatePlaces/{id}',[AdminPlacesController::class,'updatePlaces']);
+        Route::post('updatePlacesPost',[AdminPlacesController::class,'updatePlacesPost']);
+        Route::get('deletePlaces/{id}',[AdminPlacesController::class,'deletePlaces']);
+
+        Route::get('updateFeedback/{id}',[AdminFeedbackController::class,'updateFeedback']);
+        Route::get('updateFeedbackPost',[AdminFeedbackController::class,'updateFeedbackPost']);
+
+       Route::get('lockGuide/{id}',[AdminTourguideController::class,'lock']);
+       Route::get('unlockGuide/{id}',[AdminTourguideController::class,'unlock']);
+
     });
+
 });
 
-// User Routes
-Route::prefix('/user')->name('user.')->group(function () {
-    // Publicly accessible user routes
-    Route::get('/home', [UserHomeController::class, 'index'])->name('home');
-    Route::get('/sign', [UserSignController::class, 'index'])->name('sign');
-    Route::get('/register', [UserSignController::class, 'register'])->name('register');
-    Route::get('/checkRegister', [UserSignController::class, 'checkRegister'])->name('checkRegister');
-    Route::get('/checkLogin', [UserSignController::class, 'checkLogin'])->name('checkLogin');
-    Route::get('/about', [UserAboutController::class, 'index']);
-    Route::get('/contact', [UserContactController::class, 'index'])->name('contact');
-    Route::get('/contactPost', [UserContactController::class, 'contactPost']);
-    Route::get('news', [UserNewsController::class, 'index']);
-    Route::get('newsdetail/{id}', [UserNewsController::class, 'newsdetail']);
-    Route::get('allnews', [UserNewsController::class, 'allnews']);
 
-    // Protected user routes
-    Route::middleware(['auth.user'])->group(function () {
-        Route::get('/profile', [UserProfileController::class, 'index'])->name('profile');
-        Route::get('/saveProfile', [UserProfileController::class, 'saveProfile']);
-        Route::get('/profile/deleteCmt/{id}', [UserProfileController::class, 'deleteCmt']);
-        Route::get('/logout', [UserProfileController::class, 'logout']);
-        Route::get('/tour', [UserTourController::class, 'index'])->name('tour');
-        Route::get('/tourdetail/{scheid}', [UserTourdetailController::class, 'index']);
-        Route::get('tour/detailtour/feedback', [UserTourdetailController::class, 'feedback']);
-        Route::get('/payment/{scheid}', [UserPaymentContronller::class, 'index'])->name('payment');
-        Route::get('/paymentPost', [UserPaymentContronller::class, 'paymentPost']);
-    });
+
+Route::prefix('/user')->name('user.')->group(function(){
+    Route::get('/home',[UserHomeController::class,'index'])->name('home');
+
+    Route::get('/sign',[UserSignController::class,'index'])->name('sign');
+    Route::get('/register',[UserSignController::class,'register'])->name('register');
+    Route::get('/checkRegister',[UserSignController::class,'checkRegister'])->name('checkRegister');
+    Route::get('/checkLogin',[UserSignController::class,'checkLogin'])->name('checkLogin');
+
+    Route::get('/profile',[UserProfileController::class,'index'])->name('profile');
+    Route::get('/saveProfile',[UserProfileController::class,'saveProfile']);
+    Route::get('/profile/deleteCmt/{id}',[UserProfileController::class,'deleteCmt']);
+    Route::get('/logout',[UserProfileController::class,'logout']);
+
+    Route::get('news',[UserNewsController::class,'index']);
+    Route::get('newsdetail/{id}',[UserNewsController::class,'newsdetail']);
+    Route::get('allnews',[UserNewsController::class,'allnews']);
+
+    Route::get('/about',[UserAboutController::class,'index']);
+
+    Route::get('/contact',[UserContactController::class,'index'])->name('contact');
+    Route::get('/contactPost',[UserContactController::class,'contactPost']);
+
+    Route::get('/tour',[UserTourController::class,'index'])->name('tour');
+
+    // Route::get('/tourdetail/{tourid}/{scheid}',[UserTourdetailController::class,'index']);
+    Route::get('/tourdetail/{scheid}',[UserTourdetailController::class,'index']);
+    Route::get('tour/detailtour/feedback',[UserTourdetailController::class,'feedback']);
+
+    Route::get('/payment/{scheid}',[UserPaymentContronller::class,'index'])->name('payment');
+    Route::get('/paymentPost',[UserPaymentContronller::class,'paymentPost']);
+
+
+
 });
